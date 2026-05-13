3a49812afabe87d3693666f2f645f739fc174a1d
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2026-05-13 11:44:59 -0600

    Redireccionar al nuevo Sistema de Citas

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App'
-
-
-const divRoot = document.querySelector('#root')
-const root = createRoot(divRoot)
-root.render(<App />)
